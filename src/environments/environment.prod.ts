3fa503c10a5eefb7a7f740b738ba9fc44093dbd1
milan stojanov <stojanovmilan15@gmail.com>
2023-04-29 11:54:33 +0200

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBneTfCU_F0CGppWcSuHb346WPBH-oTULg",
+    authDomain: "student-angular-project.firebaseapp.com",
+    databaseURL: "https://student-angular-project-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "student-angular-project",
+    storageBucket: "student-angular-project.appspot.com",
+    messagingSenderId: "534818528858",
+    appId: "1:534818528858:web:004441ec7561fb44544263"
+  },
 };

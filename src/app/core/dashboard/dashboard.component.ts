ddd3481c68cbe37c04da371d13b0700232e35596
milan stojanov <stojanovmilan15@gmail.com>
2023-05-10 17:27:21 +0200

    studentapp

--- src/app/core/dashboard/dashboard.component.ts
+++ src/app/core/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/model/student';
+import { AuthService } from 'src/app/service/auth.service';
+import { DataService } from 'src/app/service/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +9,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
+ studentsList: Student[] = [];
+ studentObj: Student = {
+   id: '',
+   first_name: '',
+   last_name: '',
+   email: '',
+   mobile: ''
+ }
+ id: string = '';
+ first_name: string = '';
+ last_name: string = '';
+ email: string = '';
+ mobile: string = '';  
+ 
+ constructor(private service: AuthService, private data: DataService) { }
+ 
   ngOnInit(): void {
+    this.getAllStudents();
+  }
+  /* register() {
+    this.service.logout();
+  } */
+
+  getAllStudents() {
+    this.data.getAllStudents().subscribe(res => {
+      this.studentsList = res.map((e: any) => {
+        const data = e.payload.doc.data();
+        data.id = e.payload.doc.id;
+        return data;
+      })
+    }, err => {
+      alert('Error while fetching student data');
+    })
   }
 
+  resetForm() {
+    this.id = '';
+   this.first_name = '';
+   this.last_name = '';
+   this.email = '';
+   this.mobile = '';  }
+
+  addStudent() {
+    if(this.first_name == "" || this.last_name == "" || this.mobile == "" || this.email == "") {
+      alert("fill all input fields");
+      return;
+    }
+
+    this.studentObj.id = '';
+    this.studentObj.email = this.email;
+    this.studentObj.first_name = this.first_name;
+    this.studentObj.last_name = this.last_name;
+
+    this.data.addStudent(this.studentObj);
+    this.resetForm();
+  }
+  deleteStudents(student: Student) {
+    if(window.confirm('Are you sure you want to delete ' + student.first_name + '' + student.last_name + ' ?')) {
+      this.data.deleteStudent(student);
+    }
+    
+  }
 }
